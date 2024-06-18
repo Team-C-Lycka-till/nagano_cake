19db6d78b9634e789e842130396f990f85e0113c
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-06-18 02:24:36 +0000

    ［update]item新規作成の保存先推移修正　レイアウト微調整

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,7 +7,7 @@ class Admin::ItemsController < ApplicationController
   def create
   @item = Item.new(item_params)
    if @item.save
-   redirect_to admin_items_path, notice: "You have created item successfully."
+   redirect_to admin_item_path(@item), notice: "You have created item successfully."
    else
    render :new
    end
