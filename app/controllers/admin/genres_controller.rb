e1a93e72b5a053d42d53f85b96fc26a78b7e1909
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-06-21 01:06:00 +0000

    [update]管理者ヘッダーリンク作成+ページネーション修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -18,13 +18,13 @@ class Admin::GenresController < ApplicationController
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
     redirect_to admin_genres_path, notice: "You have updated genre successfully."
     else
-    flash.now[:notice] = "update genre error" 
+    flash.now[:notice] = "update genre error"
     render :edit
     end
   end
@@ -34,4 +34,3 @@ class Admin::GenresController < ApplicationController
     params.require(:genre).permit(:name)
   end
 end
-  
