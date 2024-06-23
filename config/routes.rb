e53e4d246f29163cae1e55e87b6d08680a260d95
nagi0705 <ec2-user@ip-172-31-1-56.ap-southeast-2.compute.internal>
2024-06-23 09:32:07 +0000

    解決

--- config/routes.rb
+++ config/routes.rb
@@ -45,8 +45,9 @@ Rails.application.routes.draw do
   #namespace :public do
     #get 'homes/top'
     #get 'homes/about'
- # end
- # 顧客用URL /customers/sign_in ...
+  #end
+
+  # 顧客用URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -58,41 +59,38 @@ Rails.application.routes.draw do
   }
 
   # 会員用
-    root to: 'public/homes#top'
-    get '/about' => 'public/homes#about', as: 'about'
+  root to: 'public/homes#top'
+  get '/about' => 'public/homes#about', as: 'about'
 
-    get '/customers/my_page' => 'public/customers#show', as: 'my_page'
-        get '/customers/information/edit' => 'public/customers#edit', as: 'edit_customer'
-    patch '/customers/information' => 'public/customers#update', as: 'customer'
-    get '/customers/unsubscribe' => 'public/customers#unsubscribe', as: 'unsubscribe'
-    patch '/customers/withdraw' => 'public/customers#withdraw', as: 'withdraw'
+  get '/customers/my_page' => 'public/customers#show', as: 'my_page'
+  get '/customers/information/edit' => 'public/customers#edit', as: 'edit_customer'
+  patch '/customers/information' => 'public/customers#update', as: 'customer'
+  get '/customers/unsubscribe' => 'public/customers#unsubscribe', as: 'unsubscribe'
+  patch '/customers/withdraw' => 'public/customers#withdraw', as: 'withdraw'
+  
+  delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all', as: 'destroy_all'
+  
+  post '/orders/confirm' => 'public/orders#confirm', as: 'confirm'
+  get '/orders/thanks' => 'public/orders#thanks', as: 'thanks'
+  get '/search' => 'public/items#search', as: 'search'
+  
+  scope module: :public do
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  
+  # 管理者用
+  namespace :admin do
+    root to: 'homes#top'
+    get 'customer_orders' => 'homes#customer_orders', as: 'customer_orders'
     
-        delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all', as: 'destroy_all'
-        
-        post '/orders/confirm' => 'public/orders#confirm', as: 'confirm'
-        get '/orders/thanks' => 'public/orders#thanks', as: 'thanks'
-        get '/search' => 'public/items#search', as: 'search'
-        
-        scope module: :public do
-          resources :items, only: [:index, :show]
-          resources :cart_items, only: [:index, :update, :destroy, :create]
-          resources :orders, only: [:new, :create, :index, :show]
-          resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-        end
-        
-      # 管理者用
-      namespace :admin do
-        root to: 'homes#top'    
-        get 'customer_orders' => 'homes#customer_orders', as: 'customer_orders'
-        
-        resources :items, only: [:index, :new, :create, :show, :edit, :update]
-        
-        resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, except: [:new, :create, :destroy]
+    resources :orders, only: [:show, :update]
 
-        resources :customers, except: [:new, :create, :destroy]
-              
-        resources :orders, only: [:show, :update]
-
-        patch '/admin/order_details/:id' => 'order_details#update', as: 'order_detail'
-      end
-    end      
+    patch 'order_details/:id' => 'order_details#update', as: 'order_detail'
+  end
+end
