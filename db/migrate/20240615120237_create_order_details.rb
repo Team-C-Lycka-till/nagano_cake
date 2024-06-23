d1e2c9717c2ed3fd2e7f15c36ca769142b6f2c91
nagi0705 <ec2-user@ip-172-31-1-56.ap-southeast-2.compute.internal>
2024-06-23 05:13:31 +0000

    修正

--- db/migrate/20240615120237_create_order_details.rb
+++ db/migrate/20240615120237_create_order_details.rb
@@ -1,6 +1,6 @@
-class CreateOrderDatils < ActiveRecord::Migration[6.1]
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
-    create_table :order_datils do |t|
+    create_table :order_details do |t|
       t.integer :order_id
       t.integer :item_id
       t.integer :price
